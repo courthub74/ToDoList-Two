4fe91a5e45ff44f83b31c26eabadd27b8df357c0
Courtney <cjacobs124@gmail.com>
2020-05-27 16:37:24 -0400

    Time to add Messages

--- todoTwo/forms.py
+++ todoTwo/forms.py
@@ -1,9 +1,14 @@
 from django import forms
-from .models import List
+from .models import List, Project
 
 class ListForm(forms.ModelForm):
 	class Meta:
 		model = List
 		fields = ["item", "completed"]
+
+class ProjectForm(forms.ModelForm):
+	class Meta:
+		model = Project
+		fields = ["itemproj", "completedproj"]
 
 
--- todoTwo/views.py
+++ todoTwo/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from .models import List, Project
-from .forms import ListForm
+from .forms import ListForm, ProjectForm
 
 # Create your views here.
 
 #TODOLIST
 def todo(request):
-	all_items = List.objects.all
-	return render(request, 'todo.html', {'all_items': all_items})
+	if request.method == 'POST':
+		form = ListForm(request.POST or None) #create variable called form call it ListForm and populate it with the request POST or if nothing requested, do nothing
+
+		if form.is_valid(): #if information on form is valid
+			form.save()     #save the info
+			all_items = List.objects.all
+			return render(request, 'todo.html', {'all_items': all_items})
+			
+	else:
+		all_items = List.objects.all
+		return render(request, 'todo.html', {'all_items': all_items})
 
 #PROJECTS
 def proj(request):
