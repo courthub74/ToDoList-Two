40b27734dca9e85eae2025ceadca979c33ea5664
C Jacobs <cjacobs124@gmail.com>
2020-06-30 16:09:52 -0400

    Ready for Deliverables

--- todoTwo/urls.py
+++ todoTwo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.todo, name='todo'),
+	path('practices/', views.todo, name='todo'),
 	path('delete/<list_id>', views.delete, name='delete'),
 	path('cross_off/<list_id>', views.cross_off, name='cross_off'),
 	path('uncross/<list_id>', views.uncross, name='uncross'),
@@ -13,5 +13,5 @@ urlpatterns = [
 	path('cross_off_project/<list_id>', views.cross_off_project, name='cross_off_project'),
 	path('uncross_project/<list_id>', views.uncross_project, name='uncross_project'),
 	path('projedit/<list_id>', views.projedit, name='projedit'),
-	path('basics/', views.basics, name='basics'),
+	path('', views.basics, name='basics'),
 ]
